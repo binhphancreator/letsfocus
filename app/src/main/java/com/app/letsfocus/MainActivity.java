2c2a957abd7a633e4841f89446b3d268a06bec10
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-09-26 11:22:14 +0700

    hide action bar

--- app/src/main/java/com/app/letsfocus/MainActivity.java
+++ app/src/main/java/com/app/letsfocus/MainActivity.java
@@ -29,6 +29,7 @@ private ActivityMainBinding binding;
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        getActionBar().hide();
     }
 
 }

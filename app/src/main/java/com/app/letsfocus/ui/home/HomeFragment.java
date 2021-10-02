15f5f2f145435339d7473cf98da89f0f1a819ef0
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-10-02 11:30:47 +0700

    to-to-list ui (home)

--- app/src/main/java/com/app/letsfocus/ui/home/HomeFragment.java
+++ app/src/main/java/com/app/letsfocus/ui/home/HomeFragment.java
@@ -1,42 +1,45 @@
 package com.app.letsfocus.ui.home;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProvider;
-import com.app.letsfocus.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.app.letsfocus.adapter.ToDoListAdapter;
 import com.app.letsfocus.databinding.FragmentHomeBinding;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-private FragmentHomeBinding binding;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(HomeViewModel.class);
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final ListView toDoList = binding.toDoList;
+        List<Object> listData = new ArrayList<>();
+        listData.add(new Object());
+        listData.add(new Object());
+        listData.add(new Object());
+        ToDoListAdapter adapter = new ToDoListAdapter(getActivity(), listData);
+        toDoList.setAdapter(adapter);
         return root;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;

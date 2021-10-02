955d013792f4fc7a2e19ea9c6c69ad01810cd4c5
Kim Tung Nguyen <81620234+kimtung102@users.noreply.github.com>
2021-10-02 22:22:09 +0700

    Tung/UI/Register (#5)
    * tung register form
    
    * update ui
    
    * nothing

---
+++ app/src/main/java/com/app/letsfocus/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.app.letsfocus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.letsfocus.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+}

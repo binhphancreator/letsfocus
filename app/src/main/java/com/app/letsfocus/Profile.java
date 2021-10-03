8ea3d442db84034f6e3fba548a2e615de4cf12da
thanhlieng <thanh.271020@gmail.com>
2021-10-03 19:03:16 +0700

    add ui
    Thanh

---
+++ app/src/main/java/com/app/letsfocus/Profile.java
@@ -0,0 +1,14 @@
+package com.app.letsfocus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+
+}

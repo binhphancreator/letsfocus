cd73c058066b44495eabeccf2e71032866a52a9b
luantran2k <tranluan2kyl@gmail.com>
2021-10-22 13:24:08 +0700

    create add_todo and report 1

---
+++ app/src/main/java/com/app/letsfocus/Report1Fragment.java
@@ -0,0 +1,77 @@
+package com.app.letsfocus;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.app.letsfocus.ui.notifications.NotificationsFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Report1Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Report1Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Report1Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Report1Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Report1Fragment newInstance(String param1, String param2) {
+        Report1Fragment fragment = new Report1Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_report1, container, false);
+        Button report1_btn = (Button) view.findViewById(R.id.app_usage_btn);
+
+        report1_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.nav_host_fragment_activity_main, new NotificationsFragment());
+                fr.commit();
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/app/letsfocus/ui/add_todo/AddTodoFragment.java
@@ -0,0 +1,80 @@
+package com.app.letsfocus.ui.add_todo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.app.letsfocus.R;
+import com.app.letsfocus.ui.notifications.NotificationsFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddTodoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddTodoFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AddTodoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddTodoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddTodoFragment newInstance(String param1, String param2) {
+        AddTodoFragment fragment = new AddTodoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_todo, container, false);
+
+        Button add_todo_btn = (Button) view.findViewById(R.id.save_add_todo);
+
+        add_todo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.nav_host_fragment_activity_main, new NotificationsFragment());
+                fr.commit();
+            }
+        });
+
+        return view;
+    }
+}

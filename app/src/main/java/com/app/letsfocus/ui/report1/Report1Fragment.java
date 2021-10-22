31b546f8ed7452d9a0aab7108245afa60613ed70
luantran2k <tranluan2kyl@gmail.com>
2021-10-22 13:47:47 +0700

    add folder report1

--- app/src/main/java/com/app/letsfocus/ui/report1/Report1Fragment.java
+++ app/src/main/java/com/app/letsfocus/ui/report1/Report1Fragment.java
@@ -1,4 +1,4 @@
-package com.app.letsfocus;
+package com.app.letsfocus.ui.report1;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.app.letsfocus.R;
 import com.app.letsfocus.ui.notifications.NotificationsFragment;
 
 /**

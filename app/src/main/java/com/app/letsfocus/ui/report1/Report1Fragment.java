af8879e0a925547d3d23f3bc9e282f7db2d5d60f
luantran2k <tranluan2kyl@gmail.com>
2021-10-25 10:58:50 +0700

    Add event some button

--- app/src/main/java/com/app/letsfocus/ui/report1/Report1Fragment.java
+++ app/src/main/java/com/app/letsfocus/ui/report1/Report1Fragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.app.letsfocus.R;
+import com.app.letsfocus.ui.add_timetable.AddTimeTableFragment;
+import com.app.letsfocus.ui.report2.Report2Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +20,18 @@ import com.app.letsfocus.R;
 public class Report1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_report1, container, false);
+        View view = inflater.inflate(R.layout.fragment_report1, container, false);
+
+        Button report1_btn = (Button) view.findViewById(R.id.app_usage_btn);
+        report1_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.nav_host_fragment_activity_main, new Report2Fragment());
+                fr.commit();
+            }
+        });
+
+        return view;
     }
 }

e6d44602fc1c99c627df7d0284d5dec9b5bc3f19
thanhlieng <thanh.271020@gmail.com>
2021-12-12 18:25:52 +0700

    Migrate to AndroidX

--- app/src/main/java/com/app/letsfocus/ui/activity/AppActivity.java
+++ app/src/main/java/com/app/letsfocus/ui/activity/AppActivity.java
@@ -11,7 +11,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
--- app/src/main/java/com/app/letsfocus/ui/fragment/setting/PermissionFragment.java
+++ app/src/main/java/com/app/letsfocus/ui/fragment/setting/PermissionFragment.java
@@ -4,7 +4,7 @@ package com.app.letsfocus.ui.fragment.setting;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

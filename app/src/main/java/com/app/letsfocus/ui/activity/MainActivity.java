c94a93ff26bff0c6c75c118e6c9cc3a640ea3694
Traan Hai Nam <teenhainam2603@gmail.com>
2021-12-12 16:53:05 +0700

    setting ui

--- app/src/main/java/com/app/letsfocus/ui/activity/MainActivity.java
+++ app/src/main/java/com/app/letsfocus/ui/activity/MainActivity.java
@@ -9,7 +9,6 @@ import com.app.letsfocus.R;
 import com.app.letsfocus.core.Helper;
 import com.app.letsfocus.databinding.ActivityMainBinding;
 import com.app.letsfocus.ui.fragment.home.HomeFragment;
-import com.app.letsfocus.ui.fragment.profile.ProfileFragment;
 import com.app.letsfocus.ui.fragment.report1.Report1Fragment;
 import com.app.letsfocus.ui.fragment.setting.SettingFragment;
 import com.app.letsfocus.ui.fragment.timetable.TimeTableListFragment;
@@ -20,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        com.app.letsfocus.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Helper.loadToolbar(this, R.id.my_toolbar);
+
 
         BottomNavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setOnNavigationItemSelectedListener(menuItem -> {
@@ -34,12 +34,14 @@ public class MainActivity extends AppCompatActivity {
                 loadFragment(new TimeTableListFragment());
             } else if(id == R.id.navigation_report) {
                 loadFragment(new Report1Fragment());
-            } else if(id==R.id.navigation_profile) {
+            } else if(id==R.id.navigation_setting) {
                 loadFragment(new SettingFragment());
             }
             return true;
         });
     }
+
+
 
     public void loadFragment(Fragment fragment) {
         Helper.loadFragment(R.id.nav_host_fragment_activity_main, fragment, getSupportFragmentManager());
---
+++ app/src/main/java/com/app/letsfocus/ui/fragment/setting/SettingSoundFragment.java
@@ -0,0 +1,54 @@
+package com.app.letsfocus.ui.fragment.setting;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ExpandableListView;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.app.letsfocus.R;
+import com.app.letsfocus.adapter.ReportAdapter;
+
+import java.util.ArrayList;
+
+public class SettingSoundFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting_sound, container, false);
+        RelativeLayout ringtone_btn = (RelativeLayout) view.findViewById(R.id.setting_ringtone);
+        Switch switchSound = view.findViewById(R.id.switch_sound);
+        switchSound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(switchSound.isChecked()){
+                    ringtone_btn.setVisibility(View.VISIBLE);
+                }
+                else {
+                    ringtone_btn.setVisibility(View.GONE);
+                }
+            }
+
+        });
+        Switch switchVibration = view.findViewById(R.id.switch_vibration);
+        switchVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(switchVibration.isChecked()){
+
+                }
+                else {
+
+                }
+            }
+
+        });
+        return view;
+    }
+
+}
